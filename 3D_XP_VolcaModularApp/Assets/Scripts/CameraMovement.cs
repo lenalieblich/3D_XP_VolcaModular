61a633261976d58206fac600c6cf0cdd09e3fe8c
lenalieblich <lenalieblich98@gmail.com>
2020-06-17 22:21:01 +0200

    FirstBuildVersion

---
+++ 3D_XP_VolcaModularApp/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    Camera mainCamera;
+    Vector3 camRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camRot = mainCamera.transform.eulerAngles;
+        Debug.Log(camRot);
+    }
+}
---
+++ 3D_XP_VolcaModularApp/Assets/Scripts/TouchInputManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class TouchInputManager : MonoBehaviour
+{
+    [Serializable]
+    public class FloatEvent : UnityEvent<float> { }
+
+    [SerializeField] float delay = 2; 
+
+    [SerializeField] UnityEvent onTouch;
+    [SerializeField] UnityEvent onTouchCancel;
+    [SerializeField] AnimationCurve timerUpdateCurve;
+    [SerializeField] FloatEvent onTouchTimerUpdate;
+    [SerializeField] UnityEvent onTouchTimerEnd;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            switch (Input.touches[0].phase)
+            {
+                case TouchPhase.Began:
+                    timer = 0;
+                    onTouch.Invoke();
+                    break;
+                case TouchPhase.Ended:
+                    onTouchCancel.Invoke();
+                    break;
+                default:
+                    timer += Time.deltaTime;
+                    onTouchTimerUpdate.Invoke(timerUpdateCurve.Evaluate(Mathf.InverseLerp(0, delay, timer))); 
+                    if(timer > delay)
+                    {
+                        onTouchTimerEnd.Invoke();
+                    }
+                    break;
+            }
+        }
+    }
+}
